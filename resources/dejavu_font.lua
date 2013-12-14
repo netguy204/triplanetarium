402bc9bcdaaf4fd1ab39892510d794e240eac672
Brian Taylor <el.wubo@gmail.com>
2013-12-14 15:55:24 -0500

    tiles n stuff

--- resources/dejavu_font.lua
+++ resources/dejavu_font.lua
@@ -1,159 +1,157 @@
 
 local function _font(scale)
    scale = scale or 1
-   local characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.!?,\'"\\/'
+   local characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.!?,\'"\\/+-*/%'
    local font = game:create_object('Font')
    font:load(game:atlas('resources/default'), '', characters)
    font:scale(scale)
    font:line_separation(37)
     
-   font:set_char_xadvance('a', 12)
+   font:set_char_xadvance('a', 10)
    font:set_char_yadvance('a', 0)
    font:set_char_xlead('a', 1)
    font:set_char_ylead('a', 0)
 
-   font:set_char_xadvance('b', 13)
+   font:set_char_xadvance('b', 11)
    font:set_char_yadvance('b', 0)
-   font:set_char_xlead('b', 1)
+   font:set_char_xlead('b', 2)
    font:set_char_ylead('b', 0)
 
-   font:set_char_xadvance('c', 11)
+   font:set_char_xadvance('c', 9)
    font:set_char_yadvance('c', 0)
    font:set_char_xlead('c', 1)
    font:set_char_ylead('c', 0)
 
-   font:set_char_xadvance('d', 13)
+   font:set_char_xadvance('d', 11)
    font:set_char_yadvance('d', 0)
    font:set_char_xlead('d', 1)
    font:set_char_ylead('d', 0)
 
-   font:set_char_xadvance('e', 12)
+   font:set_char_xadvance('e', 11)
    font:set_char_yadvance('e', 0)
    font:set_char_xlead('e', 1)
    font:set_char_ylead('e', 0)
 
-   font:set_char_xadvance('f', 8)
+   font:set_char_xadvance('f', 6)
    font:set_char_yadvance('f', 0)
-   font:set_char_xlead('f', 1)
+   font:set_char_xlead('f', 0)
    font:set_char_ylead('f', 0)
 
-   font:set_char_xadvance('g', 13)
+   font:set_kerning('f', '.', -1)
+
+   font:set_kerning('f', '-', -1)
+
+   font:set_char_xadvance('g', 11)
    font:set_char_yadvance('g', 0)
    font:set_char_xlead('g', 1)
    font:set_char_ylead('g', -4)
 
-   font:set_char_xadvance('h', 13)
+   font:set_char_xadvance('h', 11)
    font:set_char_yadvance('h', 0)
-   font:set_char_xlead('h', 1)
+   font:set_char_xlead('h', 2)
    font:set_char_ylead('h', 0)
 
-   font:set_char_xadvance('i', 7)
+   font:set_char_xadvance('i', 5)
    font:set_char_yadvance('i', 0)
-   font:set_char_xlead('i', 1)
+   font:set_char_xlead('i', 2)
    font:set_char_ylead('i', 0)
 
-   font:set_char_xadvance('j', 7)
+   font:set_char_xadvance('j', 5)
    font:set_char_yadvance('j', 0)
-   font:set_char_xlead('j', -1)
+   font:set_char_xlead('j', 0)
    font:set_char_ylead('j', -4)
 
-   font:set_char_xadvance('k', 12)
+   font:set_char_xadvance('k', 10)
    font:set_char_yadvance('k', 0)
-   font:set_char_xlead('k', 1)
+   font:set_char_xlead('k', 2)
    font:set_char_ylead('k', 0)
 
-   font:set_char_xadvance('l', 7)
+   font:set_char_xadvance('l', 5)
    font:set_char_yadvance('l', 0)
-   font:set_char_xlead('l', 1)
+   font:set_char_xlead('l', 2)
    font:set_char_ylead('l', 0)
 
-   font:set_char_xadvance('m', 19)
+   font:set_char_xadvance('m', 17)
    font:set_char_yadvance('m', 0)
-   font:set_char_xlead('m', 1)
+   font:set_char_xlead('m', 2)
    font:set_char_ylead('m', 0)
 
-   font:set_char_xadvance('n', 13)
+   font:set_char_xadvance('n', 11)
    font:set_char_yadvance('n', 0)
-   font:set_char_xlead('n', 1)
+   font:set_char_xlead('n', 2)
    font:set_char_ylead('n', 0)
 
-   font:set_char_xadvance('o', 12)
+   font:set_char_xadvance('o', 11)
    font:set_char_yadvance('o', 0)
    font:set_char_xlead('o', 1)
    font:set_char_ylead('o', 0)
 
-   font:set_char_xadvance('p', 13)
+   font:set_char_xadvance('p', 11)
    font:set_char_yadvance('p', 0)
-   font:set_char_xlead('p', 1)
+   font:set_char_xlead('p', 2)
    font:set_char_ylead('p', -4)
 
-   font:set_char_xadvance('q', 13)
+   font:set_char_xadvance('q', 11)
    font:set_char_yadvance('q', 0)
    font:set_char_xlead('q', 1)
    font:set_char_ylead('q', -4)
 
-   font:set_char_xadvance('r', 9)
+   font:set_char_xadvance('r', 8)
    font:set_char_yadvance('r', 0)
-   font:set_char_xlead('r', 1)
+   font:set_char_xlead('r', 2)
    font:set_char_ylead('r', 0)
 
    font:set_kerning('r', '.', -1)
 
-   font:set_kerning('r', ',', -1)
+   font:set_kerning('r', '-', -1)
 
-   font:set_char_xadvance('s', 11)
+   font:set_char_xadvance('s', 8)
    font:set_char_yadvance('s', 0)
-   font:set_char_xlead('s', 0)
+   font:set_char_xlead('s', 1)
    font:set_char_ylead('s', 0)
 
-   font:set_char_xadvance('t', 8)
+   font:set_char_xadvance('t', 7)
    font:set_char_yadvance('t', 0)
    font:set_char_xlead('t', 1)
    font:set_char_ylead('t', 0)
 
-   font:set_char_xadvance('u', 13)
+   font:set_char_xadvance('u', 11)
    font:set_char_yadvance('u', 0)
-   font:set_char_xlead('u', 1)
+   font:set_char_xlead('u', 2)
    font:set_char_ylead('u', 0)
 
-   font:set_char_xadvance('v', 10)
+   font:set_char_xadvance('v', 11)
    font:set_char_yadvance('v', 0)
    font:set_char_xlead('v', 0)
    font:set_char_ylead('v', 0)
 
    font:set_kerning('v', '.', -1)
 
-   font:set_kerning('v', ',', -1)
-
    font:set_char_xadvance('w', 16)
    font:set_char_yadvance('w', 0)
-   font:set_char_xlead('w', 0)
+   font:set_char_xlead('w', 1)
    font:set_char_ylead('w', 0)
 
    font:set_kerning('w', '.', -1)
 
-   font:set_kerning('w', ',', -1)
-
    font:set_char_xadvance('x', 11)
    font:set_char_yadvance('x', 0)
-   font:set_char_xlead('x', 0)
+   font:set_char_xlead('x', 1)
    font:set_char_ylead('x', 0)
 
-   font:set_char_xadvance('y', 10)
+   font:set_char_xadvance('y', 11)
    font:set_char_yadvance('y', 0)
    font:set_char_xlead('y', 0)
    font:set_char_ylead('y', -4)
 
-   font:set_kerning('y', '.', -1)
+   font:set_kerning('y', '.', -2)
 
-   font:set_kerning('y', ',', -1)
-
-   font:set_char_xadvance('z', 10)
+   font:set_char_xadvance('z', 9)
    font:set_char_yadvance('z', 0)
    font:set_char_xlead('z', 1)
    font:set_char_ylead('z', 0)
 
-   font:set_char_xadvance('A', 14)
+   font:set_char_xadvance('A', 12)
    font:set_char_yadvance('A', 0)
    font:set_char_xlead('A', 0)
    font:set_char_ylead('A', 0)
@@ -172,183 +170,217 @@ local function _font(scale)
 
    font:set_kerning('A', 'Y', -1)
 
-   font:set_char_xadvance('B', 15)
+   font:set_char_xadvance('B', 12)
    font:set_char_yadvance('B', 0)
-   font:set_char_xlead('B', 1)
+   font:set_char_xlead('B', 2)
    font:set_char_ylead('B', 0)
 
-   font:set_char_xadvance('C', 15)
+   font:set_kerning('B', 'Y', -1)
+
+   font:set_char_xadvance('C', 13)
    font:set_char_yadvance('C', 0)
    font:set_char_xlead('C', 1)
    font:set_char_ylead('C', 0)
 
-   font:set_char_xadvance('D', 16)
+   font:set_char_xadvance('D', 14)
    font:set_char_yadvance('D', 0)
-   font:set_char_xlead('D', 1)
+   font:set_char_xlead('D', 2)
    font:set_char_ylead('D', 0)
 
-   font:set_char_xadvance('E', 14)
+   font:set_kerning('D', 'Y', -1)
+
+   font:set_char_xadvance('E', 11)
    font:set_char_yadvance('E', 0)
-   font:set_char_xlead('E', 1)
+   font:set_char_xlead('E', 2)
    font:set_char_ylead('E', 0)
 
-   font:set_char_xadvance('F', 13)
+   font:set_char_xadvance('F', 10)
    font:set_char_yadvance('F', 0)
-   font:set_char_xlead('F', 1)
+   font:set_char_xlead('F', 2)
    font:set_char_ylead('F', 0)
 
    font:set_kerning('F', 'a', -1)
 
    font:set_kerning('F', 'e', -1)
 
-   font:set_kerning('F', 'o', -1)
+   font:set_kerning('F', 'i', -1)
+
+   font:set_kerning('F', 'r', -1)
+
+   font:set_kerning('F', 'u', -1)
+
+   font:set_kerning('F', 'y', -1)
 
    font:set_kerning('F', 'A', -1)
 
-   font:set_kerning('F', '.', -1)
+   font:set_kerning('F', '.', -2)
 
-   font:set_kerning('F', ',', -1)
-
-   font:set_char_xadvance('G', 15)
+   font:set_char_xadvance('G', 14)
    font:set_char_yadvance('G', 0)
    font:set_char_xlead('G', 1)
    font:set_char_ylead('G', 0)
 
-   font:set_char_xadvance('H', 18)
+   font:set_kerning('G', 'Y', -1)
+
+   font:set_char_xadvance('H', 14)
    font:set_char_yadvance('H', 0)
-   font:set_char_xlead('H', 1)
+   font:set_char_xlead('H', 2)
    font:set_char_ylead('H', 0)
 
-   font:set_char_xadvance('I', 8)
+   font:set_char_xadvance('I', 6)
    font:set_char_yadvance('I', 0)
-   font:set_char_xlead('I', 1)
+   font:set_char_xlead('I', 2)
    font:set_char_ylead('I', 0)
 
-   font:set_char_xadvance('J', 8)
+   font:set_char_xadvance('J', 6)
    font:set_char_yadvance('J', 0)
    font:set_char_xlead('J', -1)
    font:set_char_ylead('J', -4)
 
-   font:set_kerning('J', '.', -1)
-
-   font:set_kerning('J', ',', -1)
-
-   font:set_char_xadvance('K', 16)
+   font:set_char_xadvance('K', 12)
    font:set_char_yadvance('K', 0)
-   font:set_char_xlead('K', 1)
+   font:set_char_xlead('K', 2)
    font:set_char_ylead('K', 0)
+
+   font:set_kerning('K', 'e', -1)
+
+   font:set_kerning('K', 'o', -1)
+
+   font:set_kerning('K', 'u', -1)
 
    font:set_kerning('K', 'y', -1)
 
-   font:set_kerning('K', 'A', -1)
+   font:set_kerning('K', 'C', -1)
 
-   font:set_char_xadvance('L', 13)
+   font:set_kerning('K', 'O', -1)
+
+   font:set_kerning('K', 'T', -1)
+
+   font:set_kerning('K', '-', -1)
+
+   font:set_char_xadvance('L', 10)
    font:set_char_yadvance('L', 0)
-   font:set_char_xlead('L', 1)
+   font:set_char_xlead('L', 2)
    font:set_char_ylead('L', 0)
 
-   font:set_kerning('L', 'T', -1)
+   font:set_kerning('L', 'y', -1)
+
+   font:set_kerning('L', 'T', -2)
 
    font:set_kerning('L', 'U', -1)
 
-   font:set_kerning('L', 'V', -2)
+   font:set_kerning('L', 'V', -1)
 
    font:set_kerning('L', 'W', -1)
 
-   font:set_kerning('L', 'Y', -1)
+   font:set_kerning('L', 'Y', -2)
 
-   font:set_char_xadvance('M', 21)
+   font:set_char_xadvance('M', 16)
    font:set_char_yadvance('M', 0)
-   font:set_char_xlead('M', 1)
+   font:set_char_xlead('M', 2)
    font:set_char_ylead('M', 0)
 
-   font:set_char_xadvance('N', 17)
+   font:set_char_xadvance('N', 14)
    font:set_char_yadvance('N', 0)
-   font:set_char_xlead('N', 1)
+   font:set_char_xlead('N', 2)
    font:set_char_ylead('N', 0)
 
-   font:set_kerning('N', '.', -1)
-
-   font:set_kerning('N', ',', -1)
-
-   font:set_char_xadvance('O', 16)
+   font:set_char_xadvance('O', 14)
    font:set_char_yadvance('O', 0)
    font:set_char_xlead('O', 1)
    font:set_char_ylead('O', 0)
 
+   font:set_kerning('O', 'X', -1)
+
+   font:set_kerning('O', 'Y', -1)
+
    font:set_kerning('O', '.', -1)
 
-   font:set_kerning('O', ',', -1)
-
-   font:set_char_xadvance('P', 14)
+   font:set_char_xadvance('P', 11)
    font:set_char_yadvance('P', 0)
-   font:set_char_xlead('P', 1)
+   font:set_char_xlead('P', 2)
    font:set_char_ylead('P', 0)
+
+   font:set_kerning('P', 'a', -1)
 
    font:set_kerning('P', 'A', -1)
 
    font:set_kerning('P', '.', -2)
 
-   font:set_kerning('P', ',', -2)
-
-   font:set_char_xadvance('Q', 16)
+   font:set_char_xadvance('Q', 14)
    font:set_char_yadvance('Q', 0)
    font:set_char_xlead('Q', 1)
-   font:set_char_ylead('Q', -3)
+   font:set_char_ylead('Q', -2)
 
-   font:set_kerning('Q', '.', -1)
-
-   font:set_kerning('Q', ',', -1)
-
-   font:set_char_xadvance('R', 15)
+   font:set_char_xadvance('R', 13)
    font:set_char_yadvance('R', 0)
-   font:set_char_xlead('R', 1)
+   font:set_char_xlead('R', 2)
    font:set_char_ylead('R', 0)
 
-   font:set_char_xadvance('S', 13)
+   font:set_kerning('R', 'e', -1)
+
+   font:set_kerning('R', 'o', -1)
+
+   font:set_kerning('R', 'u', -1)
+
+   font:set_kerning('R', 'y', -1)
+
+   font:set_kerning('R', 'A', -1)
+
+   font:set_kerning('R', 'C', -1)
+
+   font:set_kerning('R', 'T', -1)
+
+   font:set_kerning('R', 'V', -1)
+
+   font:set_kerning('R', 'W', -1)
+
+   font:set_kerning('R', 'Y', -1)
+
+   font:set_kerning('R', '-', -1)
+
+   font:set_char_xadvance('S', 11)
    font:set_char_yadvance('S', 0)
    font:set_char_xlead('S', 1)
    font:set_char_ylead('S', 0)
 
-   font:set_char_xadvance('T', 13)
+   font:set_char_xadvance('T', 12)
    font:set_char_yadvance('T', 0)
    font:set_char_xlead('T', 0)
    font:set_char_ylead('T', 0)
 
-   font:set_kerning('T', 'a', -1)
+   font:set_kerning('T', 'a', -2)
 
-   font:set_kerning('T', 'c', -1)
+   font:set_kerning('T', 'c', -2)
 
-   font:set_kerning('T', 'e', -1)
+   font:set_kerning('T', 'e', -2)
 
-   font:set_kerning('T', 'o', -1)
+   font:set_kerning('T', 'o', -2)
 
-   font:set_kerning('T', 'r', -1)
+   font:set_kerning('T', 'r', -2)
 
-   font:set_kerning('T', 's', -1)
+   font:set_kerning('T', 's', -2)
 
-   font:set_kerning('T', 'u', -1)
+   font:set_kerning('T', 'u', -2)
 
-   font:set_kerning('T', 'w', -1)
+   font:set_kerning('T', 'w', -2)
 
-   font:set_kerning('T', 'y', -1)
+   font:set_kerning('T', 'y', -2)
 
    font:set_kerning('T', 'A', -1)
 
+   font:set_kerning('T', 'C', -1)
+
    font:set_kerning('T', '.', -2)
 
-   font:set_kerning('T', ',', -1)
+   font:set_kerning('T', '-', -1)
 
-   font:set_char_xadvance('U', 17)
+   font:set_char_xadvance('U', 14)
    font:set_char_yadvance('U', 0)
-   font:set_char_xlead('U', 1)
+   font:set_char_xlead('U', 2)
    font:set_char_ylead('U', 0)
 
-   font:set_kerning('U', '.', -1)
-
-   font:set_kerning('U', ',', -1)
-
-   font:set_char_xadvance('V', 14)
+   font:set_char_xadvance('V', 12)
    font:set_char_yadvance('V', 0)
    font:set_char_xlead('V', 0)
    font:set_char_ylead('V', 0)
@@ -361,17 +393,15 @@ local function _font(scale)
 
    font:set_kerning('V', 'u', -1)
 
-   font:set_kerning('V', 'y', -1)
-
    font:set_kerning('V', 'A', -1)
 
    font:set_kerning('V', '.', -2)
 
-   font:set_kerning('V', ',', -2)
+   font:set_kerning('V', '-', -1)
 
-   font:set_char_xadvance('W', 20)
+   font:set_char_xadvance('W', 19)
    font:set_char_yadvance('W', 0)
-   font:set_char_xlead('W', 0)
+   font:set_char_xlead('W', 1)
    font:set_char_ylead('W', 0)
 
    font:set_kerning('W', 'a', -1)
@@ -382,132 +412,179 @@ local function _font(scale)
 
    font:set_kerning('W', 'r', -1)
 
-   font:set_kerning('W', 'u', -1)
-
    font:set_kerning('W', 'A', -1)
 
-   font:set_kerning('W', '.', -2)
+   font:set_kerning('W', '.', -1)
 
-   font:set_kerning('W', ',', -2)
+   font:set_kerning('W', '-', -1)
 
-   font:set_char_xadvance('X', 14)
+   font:set_char_xadvance('X', 13)
    font:set_char_yadvance('X', 0)
-   font:set_char_xlead('X', 0)
+   font:set_char_xlead('X', 1)
    font:set_char_ylead('X', 0)
 
-   font:set_char_xadvance('Y', 13)
+   font:set_kerning('X', 'e', -1)
+
+   font:set_kerning('X', 'C', -1)
+
+   font:set_kerning('X', 'O', -1)
+
+   font:set_kerning('X', '-', -1)
+
+   font:set_char_xadvance('Y', 12)
    font:set_char_yadvance('Y', 0)
    font:set_char_xlead('Y', 0)
    font:set_char_ylead('Y', 0)
 
-   font:set_kerning('Y', 'a', -1)
+   font:set_kerning('Y', 'a', -2)
 
-   font:set_kerning('Y', 'e', -1)
+   font:set_kerning('Y', 'e', -2)
 
-   font:set_kerning('Y', 'o', -1)
+   font:set_kerning('Y', 'o', -2)
 
    font:set_kerning('Y', 'u', -1)
 
    font:set_kerning('Y', 'A', -1)
 
-   font:set_kerning('Y', '.', -2)
+   font:set_kerning('Y', 'C', -1)
 
-   font:set_kerning('Y', ',', -2)
+   font:set_kerning('Y', 'O', -1)
+
+   font:set_kerning('Y', '.', -3)
+
+   font:set_kerning('Y', '-', -2)
 
    font:set_char_xadvance('Z', 13)
    font:set_char_yadvance('Z', 0)
    font:set_char_xlead('Z', 1)
    font:set_char_ylead('Z', 0)
 
-   font:set_char_xadvance('0', 13)
+   font:set_char_xadvance('0', 11)
    font:set_char_yadvance('0', 0)
    font:set_char_xlead('0', 1)
    font:set_char_ylead('0', 0)
 
-   font:set_char_xadvance('1', 13)
+   font:set_char_xadvance('1', 11)
    font:set_char_yadvance('1', 0)
    font:set_char_xlead('1', 2)
    font:set_char_ylead('1', 0)
 
-   font:set_char_xadvance('2', 13)
+   font:set_char_xadvance('2', 11)
    font:set_char_yadvance('2', 0)
    font:set_char_xlead('2', 1)
    font:set_char_ylead('2', 0)
 
-   font:set_char_xadvance('3', 13)
+   font:set_char_xadvance('3', 11)
    font:set_char_yadvance('3', 0)
    font:set_char_xlead('3', 1)
    font:set_char_ylead('3', 0)
 
-   font:set_char_xadvance('4', 13)
+   font:set_char_xadvance('4', 11)
    font:set_char_yadvance('4', 0)
    font:set_char_xlead('4', 1)
    font:set_char_ylead('4', 0)
 
-   font:set_char_xadvance('5', 13)
+   font:set_char_xadvance('5', 11)
    font:set_char_yadvance('5', 0)
    font:set_char_xlead('5', 1)
    font:set_char_ylead('5', 0)
 
-   font:set_char_xadvance('6', 13)
+   font:set_char_xadvance('6', 11)
    font:set_char_yadvance('6', 0)
    font:set_char_xlead('6', 1)
    font:set_char_ylead('6', 0)
 
-   font:set_char_xadvance('7', 13)
+   font:set_char_xadvance('7', 11)
    font:set_char_yadvance('7', 0)
    font:set_char_xlead('7', 1)
    font:set_char_ylead('7', 0)
 
-   font:set_char_xadvance('8', 13)
+   font:set_char_xadvance('8', 11)
    font:set_char_yadvance('8', 0)
    font:set_char_xlead('8', 1)
    font:set_char_ylead('8', 0)
 
-   font:set_char_xadvance('9', 13)
+   font:set_char_xadvance('9', 11)
    font:set_char_yadvance('9', 0)
    font:set_char_xlead('9', 1)
    font:set_char_ylead('9', 0)
 
-   font:set_char_xadvance('.', 5)
+   font:set_char_xadvance('.', 6)
    font:set_char_yadvance('.', 0)
-   font:set_char_xlead('.', 1)
+   font:set_char_xlead('.', 2)
    font:set_char_ylead('.', 0)
 
-   font:set_char_xadvance('!', 8)
+   font:set_char_xadvance('!', 7)
    font:set_char_yadvance('!', 0)
-   font:set_char_xlead('!', 2)
+   font:set_char_xlead('!', 3)
    font:set_char_ylead('!', 0)
 
-   font:set_char_xadvance('?', 11)
+   font:set_char_xadvance('?', 10)
    font:set_char_yadvance('?', 0)
    font:set_char_xlead('?', 1)
    font:set_char_ylead('?', 0)
 
    font:set_char_xadvance(',', 6)
    font:set_char_yadvance(',', 0)
-   font:set_char_xlead(',', 0)
-   font:set_char_ylead(',', -3)
+   font:set_char_xlead(',', 1)
+   font:set_char_ylead(',', -2)
 
-   font:set_char_xadvance("'", 6)
+   font:set_char_xadvance("'", 4)
    font:set_char_yadvance("'", 0)
-   font:set_char_xlead("'", 2)
+   font:set_char_xlead("'", 1)
    font:set_char_ylead("'", 8)
 
-   font:set_char_xadvance('"', 9)
+   font:set_char_xadvance('"', 8)
    font:set_char_yadvance('"', 0)
-   font:set_char_xlead('"', 2)
+   font:set_char_xlead('"', 1)
    font:set_char_ylead('"', 8)
 
-   font:set_char_xadvance('\\', 7)
+   font:set_char_xadvance('\\', 6)
    font:set_char_yadvance('\\', 0)
    font:set_char_xlead('\\', 0)
    font:set_char_ylead('\\', -2)
 
-   font:set_char_xadvance('/', 7)
+   font:set_char_xadvance('/', 6)
    font:set_char_yadvance('/', 0)
    font:set_char_xlead('/', 0)
    font:set_char_ylead('/', -2)
+
+   font:set_char_xadvance('+', 15)
+   font:set_char_yadvance('+', 0)
+   font:set_char_xlead('+', 2)
+   font:set_char_ylead('+', 0)
+
+   font:set_char_xadvance('-', 7)
+   font:set_char_yadvance('-', 0)
+   font:set_char_xlead('-', 1)
+   font:set_char_ylead('-', 4)
+
+   font:set_kerning('-', 'J', 1)
+
+   font:set_kerning('-', 'T', -1)
+
+   font:set_kerning('-', 'V', -1)
+
+   font:set_kerning('-', 'W', -1)
+
+   font:set_kerning('-', 'X', -1)
+
+   font:set_kerning('-', 'Y', -2)
+
+   font:set_char_xadvance('*', 9)
+   font:set_char_yadvance('*', 0)
+   font:set_char_xlead('*', 1)
+   font:set_char_ylead('*', 5)
+
+   font:set_char_xadvance('/', 6)
+   font:set_char_yadvance('/', 0)
+   font:set_char_xlead('/', 0)
+   font:set_char_ylead('/', -2)
+
+   font:set_char_xadvance('%', 17)
+   font:set_char_yadvance('%', 0)
+   font:set_char_xlead('%', 1)
+   font:set_char_ylead('%', 0)
 
    return font
 end
--- resources/init.lua
+++ resources/init.lua
@@ -11,9 +11,11 @@ local DynO = require 'DynO'
 local czor = game:create_object('Compositor')
 local Tile
 local Board
+local Hand
+local Deck
 
-local tw = 84
-local th = 84
+local tw = 64
+local th = 64
 local nw = 8
 local nh = 8
 local padding = 8
@@ -22,55 +24,284 @@ function background()
    czor:clear_with_color(util.rgba(255,255,255,255))
 end
 
+local function animate_between(item)
+   local s = (1 - item.rdt / item.dt)
+   local dx = item.stop - item.start
+   local x = item.start + dx * s
+
+   item.go:pos(x)
+   item.rdt = item.rdt - world:dt()
+   if item.rdt <= 0 then
+      item.go:pos(item.stop)
+      return false
+   else
+      return true
+   end
+end
+
+local Sequence = oo.class(oo.Object)
+function Sequence:init()
+   self.queue = {}
+   self.running = false
+end
+
+function Sequence:animate_between(go, start, stop, dt)
+   table.insert(self.queue, {fn=animate_between, arg={go=go, start=start, stop=stop, dt=dt, rdt=dt}})
+end
+
+function Sequence:next(fn, ...)
+   local lfn = function(item)
+      return fn(unpack(item))
+   end
+   table.insert(self.queue, {fn=fn, item={...}})
+end
+
+function Sequence:start()
+   if util.empty(self.queue) or self.running then
+      return
+   end
+
+   self.running = true
+   local comp
+
+   local thread = function()
+      local item = self.queue[1]
+      if not item.fn(item.arg) then
+         table.remove(self.queue, 1)
+         if util.empty(self.queue) then
+            comp:delete_me(1)
+            self.running = false
+            return
+         end
+      end
+   end
+
+   comp = stage:add_component('CScripted', {update_thread=util.fthread(thread)})
+end
+
 local DIRECTION = {
-   'NORTH',
-   'SOUTH',
-   'EAST',
-   'WEST'
+   NORTH = {angle=0, offset=vector.new({0,1})},
+   SOUTH = {angle=math.pi, offset=vector.new({0,-1})},
+   EAST =  {angle=-math.pi/2, offset=vector.new({1,0})},
+   WEST =  {angle=math.pi/2, offset=vector.new({-1,0})}
 }
 
 Tile = oo.class(DynO)
-function Tile:init(center, n)
+function Tile:init(center, n, d)
    DynO.init(self, center)
 
    local go = self:go()
    go:body_type(constant.STATIC)
-
-   local font = font_factory(1)
-   local w = tw - padding/2
-   local h = th - padding/2
-   local str = tostring(n)
-   local sw = font:string_width(str)
-   local sh = font:line_height(str)
-   go:add_component('CDrawText', {font=font, color={1,1,1,1}, message=tostring(n),
-                                  offset={-sw/2, -sh/8}})
-   go:add_component('CTestDisplay', {w=w, h=h})
+   self:fpos(center)
+   self.n = n
+   self.d = d
+   self:hide()
 end
 
-Board = oo.class(oo.Object)
-function Board:init(offset)
-   local tiles = {}
+function Tile:direction(name)
+   if not name then
+      return DIRECTION[self.d]
+   else
+      self.d = name
+      if self.t3 then
+         self.t3:angle_offset(DIRECTION[self.d].angle)
+      end
+   end
+end
 
-   for yy = 1,nh do
-      for xx = 1,nw do
-         local tile = Tile(offset + {xx * tw, yy * th}, xx + (yy-1) * nw)
-         table.insert(tiles, tile)
+function Tile:show()
+   local font = font_factory(2)
+   local str = tostring(self.n)
+   local sw = font:string_width(str)
+   local sh = font:line_height(str)
+
+   self:hide(true)
+   local go = self:go()
+   self.t1 = go:add_component('CDrawText', {font=font, color={0,0,0,1}, message=str,
+                                            offset={-sw/2, -sh/8-2}})
+   self.t2 = go:add_component('CDrawText', {font=font, color={1,1,1,1}, message=str,
+                                            offset={-sw/2, -sh/8}})
+
+
+   local dir = DIRECTION[self.d]
+   local _art = game:atlas_entry(constant.ATLAS, 'tile')
+   self.t3 = go:add_component('CStaticSprite', {entry=_art,
+                                                angle_offset=dir.angle})
+end
+
+function Tile:hide(fully)
+   local parts = {'t1', 't2', 't3'}
+   for ii=1,#parts do
+      if self[parts[ii]] then
+         self[parts[ii]]:delete_me(1)
+         self[parts[ii]] = nil
       end
    end
 
-   self.tiles = tiles
+   if not fully then
+      local go = self:go()
+      local _art = game:atlas_entry(constant.ATLAS, 'tile_back')
+      self.t3 = go:add_component('CStaticSprite', {entry=_art})
+   end
 end
 
+function Tile:vpos(pos)
+   if not pos then
+      return self._vpos
+   else
+      self._vpos = vector.new(pos)
+   end
+end
 
+function Tile:pos(pos)
+   if not pos then
+      return vector.new(self:go():pos())
+   else
+      self:go():pos(pos)
+   end
+end
+
+function Tile:fpos(pos)
+   self:vpos(pos)
+   self:pos(pos)
+end
+
+Board = oo.class(oo.Object)
+function Board:init(offset, deck)
+   self.offset = offset
+   local tiles = {}
+   local tile = deck:draw_number()
+   tiles[{1,nh}] = tile
+   tile:direction('EAST')
+
+   local anim = Sequence()
+   anim:animate_between(tile:go(), tile:pos(), self:loc2center({1,nh}), .3)
+   anim:next(tile:bind('show'))
+   anim:next(tile:bind('fpos'), self:loc2center({1,nh}))
+   anim:start()
+
+   self.tiles = tiles
+   self.nw = nw
+   self.nh = nh
+end
+
+function Board:loc2center(loc)
+   return self.offset + {loc[1] * tw, loc[2] * th}
+end
+
+function Board:worldpos2loc(pos)
+   pos = pos - self.offset
+   local xx = util.round(pos[1] / tw)
+   local yy = util.round(pos[2] / th)
+   if xx < 1 or xx > nw or yy < 1 or yy > nh then
+      return nil
+   else
+      return {xx, yy}
+   end
+end
+
+function Board:worldpos2center(pos)
+   local loc = self:worldpos2loc(pos)
+   if not loc then
+      return nil
+   end
+
+   return self.offset + {loc[1] * tw, loc[2] * th}
+end
+
+function Board:worldpos2rect(pos)
+   local cc = self:worldpos2center(pos)
+   if not cc then
+      return nil
+   end
+
+   return Rect.centered(cc, tw, th)
+end
+
+Hand = oo.class(oo.Object)
+function Hand:init(deck, max, offset)
+   self.max = max
+   self.tiles = {}
+   self.offset = vector.new(offset)
+
+   local anim = Sequence()
+   for ii=1,max do
+      local tile = deck:draw()
+      self.tiles[ii] = tile
+      anim:animate_between(tile:go(), tile:pos(), self:idx2center(ii), .3)
+      anim:next(tile:bind('show'))
+      anim:next(tile:bind('fpos'), self:idx2center(ii))
+   end
+   anim:start()
+end
+
+function Hand:idx2center(ii)
+   return self.offset + {ii * tw, 0}
+end
+
+Deck = oo.class(oo.Object)
+function Deck:init(offset)
+   self.offset = vector.new(offset)
+
+   local values = {2, 3, 4, 4, 5, 5, 7, 9, 9,
+                   '+', '+', '+', '+', '-', '-', '-'}
+   values = util.rand_shuffle(values)
+
+   self.tiles = {}
+   for ii=1,#values do
+      local tile = Tile(self.offset, values[ii], 'NORTH')
+      tile:hide()
+      table.insert(self.tiles, tile)
+   end
+end
+
+function Deck:draw()
+   if util.empty(self.tiles) then
+      return nil
+   else
+      local item = self.tiles[1]
+      table.remove(self.tiles, 1)
+      return item
+   end
+end
+
+function Deck:draw_number()
+   for ii=1,#self.tiles do
+      if util.isnumber(self.tiles[ii].n) then
+         local tile = self.tiles[ii]
+         table.remove(self.tiles, ii)
+         return tile
+      end
+   end
+end
+
+function Deck:pos()
+   return self.offset
+end
 
 function init()
    util.install_basic_keymap()
+   util.install_mouse_map()
    world:gravity({0,0})
 
    local cam = stage:find_component('Camera', nil)
    cam:pre_render(util.fthread(background))
 
-   local board = Board(vector.new({-16, -16}))
+   local deck = Deck({800,600})
+   local hand = Hand(deck, 5, {800,200})
+   local board = Board(vector.new({-16, -16}), deck)
+
+   local hl = stage:add_component('CTestDisplay', {w=tw, h=th,
+                                                   color={1,1,1,.3}})
+   local input = function()
+      local mouse = util.mouse_state()
+      local rect = board:worldpos2rect(mouse)
+      if rect then
+         hl:offset(rect:center())
+      end
+   end
+   stage:add_component('CScripted', {update_thread=util.fthread(input)})
+
 end
 
 function game_init()
